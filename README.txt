d96c95a131f51addc921b4b8cbff764a7d560039
Kode Vasse <kodevasse@outlook.com>
2022-01-31 01:54:06 +0100

    first commit

---
+++ README.txt
@@ -0,0 +1,9 @@
+PDPkrypto NFT test collection
+
+React - Next
+
+Project include NFT solidity contract
+
+
+npm run dev
+
---
+++ hardhat-tutorial/constants/index.js
@@ -0,0 +1,6 @@
+// Address of the Whitelist Contract that you deployed
+const WHITELIST_CONTRACT_ADDRESS = "0xF5b196435CA084F0BF9ceC1B2fdF44b21304574B";
+// URL to extract Metadata for a Crypto Dev NFT
+const METADATA_URL = "https://nft-collection-sneh1999.vercel.app/api/";
+
+module.exports = { WHITELIST_CONTRACT_ADDRESS, METADATA_URL };
